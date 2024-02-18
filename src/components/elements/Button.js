5f043530fe6f8f7de0648d171aa3cb96e62e89ff
Mayank <mayankmahajan936@gmail.com>
2024-02-18 11:23:27 +0530

    server setup

--- src/components/elements/Button.js
+++ src/components/elements/Button.js
@@ -1,5 +1,44 @@
-const Button = ({ Children }) => {
-  return <button>{Children}</button>;
+import clsx from "clsx";
+import { Children } from "react";
+
+function getClassName({ className }) {
+  return clsx(
+    "text-white text-lg font-bold rounded-xl transition duration-300 cursor-pointer focus:outline-none focus:ring-2 focus:ring-opacity-50",
+    className
+  );
+}
+
+const sizes = {
+  small: "px-4 py-3",
+  medium: "px-6 py-4",
+  large: "w-full px-4 py-3",
+};
+
+const variants = {
+  primary: "bg-marigold focus:ring-marigold",
+  secondary: "bg-tomato focus:ring-tomato",
+  dark: "bg-black focus:ring-white",
+};
+
+const Button = ({
+  Children,
+  className,
+  size = "small",
+  variant = "primary",
+  ...rest
+}) => {
+  return (
+    <button
+      className={clsx(
+        sizes[size],
+        variant[variant],
+        getClassName({ className })
+      )}
+      {...rest}
+    >
+      {Children}
+    </button>
+  );
 };
 
 export default Button;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["ClashDisplay-Regular", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        tomato: "#ES0914",
+        marigold: "#ffbe0b",
+      },
+    },
   },
   plugins: [],
 };
